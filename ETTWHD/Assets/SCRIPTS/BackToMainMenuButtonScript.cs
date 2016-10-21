4a8c75b92ef51b92c9befe5f6c229e6d6d3cd62a
greenjack3 <jacyna20@gmail.com>
2016-10-21 21:44:34 +0200

    Main Menu returning buttons
    added Back to main menu buttons to Settings and Notebook scenes

---
+++ ETTWHD/Assets/SCRIPTS/BackToMainMenuButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenuButtonScript : MonoBehaviour
+{
+
+	public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
