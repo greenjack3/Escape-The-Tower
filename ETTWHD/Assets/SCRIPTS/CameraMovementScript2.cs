d01e88ad11eb90898ee4711dc54a7e62b5cff6cf
greenjack3 <jacyna20@gmail.com>
2016-10-22 11:49:44 +0200

    some CameraMovement
    blabla stupid scripts

---
+++ ETTWHD/Assets/SCRIPTS/CameraMovementScript2.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovementScript2 : MonoBehaviour
+{
+    private float speed = 2.0f;
+    private float zoomSpeed = 2.0f;
+
+    public float minX = -360.0f;
+    public float maxX = 360.0f;
+
+    public float minY = -45.0f;
+    public float maxY = 45.0f;
+
+    public float sensX = 100.0f;
+    public float sensY = 100.0f;
+
+    float rotationY = 0.0f;
+    float rotationX = 0.0f;
+
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        transform.Translate(0, scroll * zoomSpeed, scroll * zoomSpeed, Space.World);
+
+       
+
+        rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
+        rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, minY, maxY);
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+        if (Input.GetMouseButton(1))
+        {
+            if (Input.GetAxis("Mouse X") > 0)
+            {
+                transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+         0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+            }
+
+            else if (Input.GetAxis("Mouse X") < 0)
+            {
+                transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+         0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+
+            }
+        }
+
+
+    }
+}
