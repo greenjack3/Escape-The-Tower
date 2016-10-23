cc9b9b092da5a43c393b5f16635a0439b56def06
greenjack3 <jacyna20@gmail.com>
2016-10-23 12:53:45 +0200

    OncClick character movement
    Added

---
+++ ETTWHD/Assets/SCRIPTS/CharacterMovementScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovementScript : MonoBehaviour
+{
+    private bool flag = false;
+
+    private Vector3 endPoint;
+
+    public float duration = 50.0f;
+
+    private float yAxis;
+
+    void start()
+    {
+        yAxis = gameObject.transform.position.y;
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hit))
+            {
+                flag = true;
+                endPoint = hit.point;
+                endPoint.y = yAxis;
+                
+            }
+        }
+        if(flag && !Mathf.Approximately(gameObject.transform.position.magnitude, endPoint.magnitude))
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, endPoint, 1 / (duration * (Vector3.Distance(gameObject.transform.position, endPoint))));
+        }
+        else if (flag && Mathf.Approximately(gameObject.transform.position.magnitude, endPoint.magnitude))
+        {
+            flag = false;
+            Debug.Log("i am here");
+        }
+
+    }
+
+	
+}
