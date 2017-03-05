8d183e79d5b0dae2bb62356f6cd505d118d5904f
melangemen <xigorx@o2.pl>
2017-03-05 16:08:49 +0100

    TurnBasedCombat menager nie działa, tylko Elven Wynch ma zaaplikowane DetectionLogic

---
+++ ETTWHD/Assets/DetectionLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionLogic : MonoBehaviour
+{
+
+    public float fieldOfViewDegrees;
+    public float visibilityDistance;
+    private Vector3 playerPosition;
+    
+    void Start()
+    {
+
+    }
+
+    public bool CanSeePlayer()
+    {
+        RaycastHit hit;
+        GameObject player = GameObject.FindWithTag("Player");
+        playerPosition = player.transform.position;
+        Vector3 rayDirection = player.transform.position - transform.position;
+
+        if ((Vector3.Angle(rayDirection, transform.forward)) <= fieldOfViewDegrees * 0.5f)
+        {
+            if (Physics.Raycast(transform.position, rayDirection, out hit, visibilityDistance))
+            {
+                return (hit.transform.CompareTag("Player"));
+
+                Debug.Log("Player Spotted");
+            }
+        }
+
+        return false;
+    }
+}
---
+++ ETTWHD/Assets/TurnBasedCombatStateMenager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBasedCombatStateMenager : MonoBehaviour {
+
+    private bool playerDetected;
+    public enum BattleStates
+    {
+        OUTOFCOMBAT,
+        START,
+        PLAYERCHOICE,
+        ENEMYCHOICE
+    }
+
+    private BattleStates currentState;
+
+    void Start()
+    {
+        currentState = BattleStates.OUTOFCOMBAT;
+        playerDetected = false;
+    }
+
+    void Update()
+    {
+       // playerDetected = gameObject.GetComponent<DetectionLogic>().CanSeePlayer();
+
+        if (playerDetected == false)
+        {
+            currentState = BattleStates.OUTOFCOMBAT;
+            Debug.Log("OutofCombat");
+        }
+        else
+        {
+            switch (currentState)
+            {
+                case (BattleStates.START):
+                    Debug.Log("CombatStateStart");
+                    break;
+                case (BattleStates.PLAYERCHOICE):
+                    Debug.Log("CombatStatePlayerChoice");
+                    break;
+                case (BattleStates.ENEMYCHOICE):
+                    Debug.Log("CombatStateStartEnemyChoice");
+                    break;
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("NEXT STATE"))
+        {
+            if (currentState == BattleStates.OUTOFCOMBAT)
+            {
+                currentState = BattleStates.START;
+            }
+            else if (currentState == BattleStates.START)
+            {
+                currentState = BattleStates.PLAYERCHOICE;
+            }
+            else if (currentState == BattleStates.PLAYERCHOICE)
+            {
+                currentState = BattleStates.ENEMYCHOICE;
+            }
+            else if (currentState == BattleStates.ENEMYCHOICE)
+            {
+                currentState = BattleStates.START;
+            }
+        }
+    }
+}
