0e80ef6af8d26d12546620a5e7785b643a199827
greenjack3 <jacyna20@gmail.com>
2017-03-10 22:56:22 +0100

    Trochę na około bo nie umiem programować animiacji ale animacja sie odpala, skrzynka znika i zostawia przedmiot

--- ETTWHD/Assets/SkrzynkoOtwieracz.cs
+++ ETTWHD/Assets/SkrzynkoOtwieracz.cs
@@ -5,13 +5,37 @@ using UnityEngine;
 public class SkrzynkoOtwieracz : MonoBehaviour
 {
     public GameObject[] bronie;
-
+    bool otwarte = false;
+    float x = 0;
+   
 
     public void Otwieraj()
     {
-        GameObject bron = bronie[Random.Range(0, bronie.Length)];
-        GameObject instance = Instantiate(bron, transform.position, transform.rotation);
-        gameObject.SetActive(false);
+        
+       
+        GetComponentInParent<Animator>().Play("ChestAnim");
+        otwarte = true;
+      
+        
+            
+       
+     }
+     void Update()
+    {
+        if (otwarte == true)
+        {
+            x++;
+        }
+
+      
+        if (x > 62)
+        {
+            GameObject bron = bronie[Random.Range(0, bronie.Length)];
+            GameObject instance = Instantiate(bron, transform.position, transform.rotation);
+            Debug.Log("prezent");
+
+            gameObject.SetActive(false);
+           
+        }
     }
-	
 }
